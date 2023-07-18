775ba5b4f962b2103649d968d5336c67d65916d9
Alex Yoon <43189680+alexyoon97@users.noreply.github.com>
2023-07-17 23:10:52 -0700

    update

--- q4.py
+++ q4.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-# Purpose: To label unsegment voice data, by calculating all possible path to get right output by summing over the probabilty of all possible sequence
-#           CTC can inference label the sequence by predicting.
-# Logic:  By excuting dynamic programming to find best possible path with time efficinty and re-using the data
+# Purpose: To label unsegmented voice data, calculate all possible paths and sum their probabilities for the correct output.
+# Logic: Executing dynamic programming to efficiently find the best path while reusing data
 def ctc_dynamic_programming(inputs, labels, time_seq, num_label):
     
 # Initialize the forward variables
@@ -25,10 +24,10 @@ def ctc_dynamic_programming(inputs, labels, time_seq, num_label):
     return output_probs
 
 
-# Purpose: To determine all next possible path from time and state
-#          by implemending forward algorithm we are able to calcualte alpha value by multiplying probability of evey time sequence
-#        Alpha value can be used on next time sequence to find probabilty with less calcuation by re-using alpha t -1 value
-# Logic: Iterate over the time steps from 1 to T and length of labels, update the alpha values based on preivous alpha value
+# Purpose: To determine all next possible paths from time and state
+#   By implementing a forward algorithm we are able to calculate the alpha value by multiplying the probability of every time sequence
+#   Alpha value can be used on next time sequence to find probability with less calculation by re-using alpha t -1 value
+# Logic: Iterate over the time steps from 1 to T and length of labels, update the alpha values based on the previous alpha value
 def forward(alpha, T, C, inputs, labels):
     for t in range(1, T):
         for c in range(C):
@@ -45,11 +44,11 @@ def backward(beta, T, C, inputs, labels):
                 beta[t, c] = beta[t+1, c] + beta[t+1, c+1] + inputs[t, labels[c]]
 
 
-# Purpose: The value of output will be too small after implement forward-backward algorithm, to prevent underflow rescale the value
+# Purpose: The value of output will be too small after implementing a forward-backward algorithm, to prevent underflow rescale of the value
 # def rescaling():
 
-# Purpose: To filter out e and repeative character from forward-backward algorithm output
-# Logic: Get total probability of all paths going though sequence at timestep t by multiplying alpha and beta value
+# Purpose: To filter out e and repetitive characters from forward-backward algorithm output
+# Logic: Get the total probability of all paths going through sequence at timestep t by multiplying the alpha and beta value
 # def beam_search_decoding():
 
 inputs = np.array([[0.1, 0.2, 0.3, 0.4],
@@ -63,6 +62,6 @@ time_seq, num_label = inputs.shape[0], labels.shape[0]
 result = ctc_dynamic_programming(inputs, label_indices, time_seq, num_label)
 
 #Prefix decoding method
-# Convert the output probabilities back to alphabet labels by picking most highest probability.
+# Convert the output probabilities back to alphabet labels by picking highest probability.
 output_labels = np.array([labels[np.argmax(result[t])] for t in range(result.shape[0])])
 print(output_labels)
